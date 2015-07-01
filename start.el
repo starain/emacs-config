2b61bfa1ccf657a37bf0fe6a78c5fd3dc2d0244f
Yi Zhang <starain.zhang@gmail.com>
2015-06-30 17:30:15 -0700

    Add hot key list in README file. Remove binding in term for C-p and C-n to use default terminal behavior

--- start.el
+++ start.el
@@ -114,12 +114,29 @@
 (load-file (concat yi-thirdparty-dir "multi-term.el"))
 (require 'multi-term)
 (setq multi-term-program "/bin/zsh")
+;; yanking / pasting text into a terminal buffer
+;;(add-hook 'term-mode-hook
+;;  (lambda ()
+;;    (define-key term-raw-map (kbd "C-y") 'term-paste)))
+;; cycle through terminals with M-[ and M-]
+;; Applies everywhere
 
+;; Only applies to term mode
+;; (add-hook 'term-mode-hook
+;;   (lambda ()
+;;     (add-to-list 'term-bind-key-alist '("M-[" . multi-term-prev))
+;;     (add-to-list 'term-bind-key-alist '("M-]" . multi-term-next))))
+;; set buffer size. 0 means no limit.
+(add-hook 'term-mode-hook
+  (lambda ()
+    (setq term-buffer-maximum-size 10000)))
+(global-set-key (kbd "M-[") 'multi-term-prev)
+(global-set-key (kbd "M-]") 'multi-term-next)
 (when (require 'term nil t) ; only if term can be loaded..
   (setq term-bind-key-alist
         (list (cons "C-c C-c" 'term-interrupt-subjob)
-              (cons "C-p" 'previous-line)
-              (cons "C-n" 'next-line)
+;;              (cons "C-p" 'previous-line)
+;;              (cons "C-n" 'next-line)
               (cons "M-f" 'term-send-forward-word)
               (cons "M-b" 'term-send-backward-word)
               (cons "C-c C-j" 'term-line-mode)
