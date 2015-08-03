ae1218f1f0d57075f02e7aca27d3c4e9f647382d
Yi Zhang <starain.zhang@gmail.com>
2015-08-03 00:24:26 -0700

    use tab for helm persistent action

--- settings/key-bindings.el
+++ settings/key-bindings.el
@@ -8,6 +8,9 @@
 (global-set-key (kbd "C-c k") 'helm-show-kill-ring)
 (global-set-key (kbd "C-x b") 'helm-mini)
 (global-set-key (kbd "C-x C-f") 'helm-find-files)
+(define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebind tab to run persistent action
+(define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB works in terminal
+(define-key helm-map (kbd "C-z")  'helm-select-action) ; list actions using C-z
 
 ;; swbuff
 (global-set-key (kbd "M-9") 'swbuff-switch-to-previous-buffer)
